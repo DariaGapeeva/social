dcae8c6e8430330c88deee0d187eb178ec03b318
DariaGapeeva <d.gapeeva@gmail.com>
2020-11-17 15:03:34 +0700

    Updates

---
+++ precache-manifest.fe0f4e82263d85540b076292f45ab88e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9acb70b8b6e133c87fa296358766ed37",
+    "url": "/social/index.html"
+  },
+  {
+    "revision": "ccd118088713e2207ce8",
+    "url": "/social/static/css/main.77ee2cb8.chunk.css"
+  },
+  {
+    "revision": "2ec4660dd9d467bb1d66",
+    "url": "/social/static/js/2.b3946e39.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/social/static/js/2.b3946e39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccd118088713e2207ce8",
+    "url": "/social/static/js/main.aa558088.chunk.js"
+  },
+  {
+    "revision": "49495d0cf50a09d7298b",
+    "url": "/social/static/js/runtime-main.e2a91f51.js"
+  },
+  {
+    "revision": "29c5063c5eda1d6fb50cc77e6ccb8967",
+    "url": "/social/static/media/1.29c5063c.jpg"
+  },
+  {
+    "revision": "7b10dc3e985ee12aa1925d0b6c1f3049",
+    "url": "/social/static/media/Japan.7b10dc3e.jpeg"
+  },
+  {
+    "revision": "39f57bc8b3a4f70f15dc1ec1a673ba77",
+    "url": "/social/static/media/castle.39f57bc8.jpg"
+  },
+  {
+    "revision": "67a57fe198a523c4507def46082ff57e",
+    "url": "/social/static/media/lavender.67a57fe1.jpg"
+  },
+  {
+    "revision": "59b212f9822150c7033d1abb79772233",
+    "url": "/social/static/media/mountains.59b212f9.jpg"
+  },
+  {
+    "revision": "f21d01f7d706bc7b9b31fdcf025d0834",
+    "url": "/social/static/media/notepad.f21d01f7.png"
+  },
+  {
+    "revision": "930aeb5315aadb9273f7b27359d9d7a2",
+    "url": "/social/static/media/profile1.930aeb53.png"
+  }
+]);
